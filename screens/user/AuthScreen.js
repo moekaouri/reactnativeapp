b4d4706dc74a58f79cdcdf3f29025bc52f8de8f4
Mohammad Kaouri <moe@fusionsecond.com>
2021-04-25 17:28:20 +0300

    changing color to check if commit is working

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -107,7 +107,7 @@ const AuthScreen = props => {
       keyboardVerticalOffset={50}
       style={styles.screen}
     >
-      <LinearGradient colors={['#ffedff', '#ffe3ff']} style={styles.gradient}>
+      <LinearGradient colors={['black', 'black']} style={styles.gradient}>
         <Card style={styles.authContainer}>
           <ScrollView>
             <Input
